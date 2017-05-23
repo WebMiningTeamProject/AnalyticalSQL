223c3815eb33899fe21564cba4d7374e464d323f
janothan <janothan@users.noreply.github.com>
2017-05-23 12:21:19 +0200

    Update analytical_views.sql

--- analytical_views.sql
+++ analytical_views.sql
@@ -4,19 +4,19 @@
 create view average_sentiment_per_cluster_and_provider
 as
 SELECT 
-    AVG(sl.sentiment) 'AVG Sentiment',
-    rp.root_name 'Root News Provider',
-    c.cluster_name 'Cluster Name'
+	AVG(sl.sentiment) 'AVG Sentiment',
+	rp.root_name 'Root News Provider',
+	c.cluster_name 'Cluster Name'
 FROM
-    NewsArticles a
-        INNER JOIN
-    ClusterAssignment ca ON a.source_uri = ca.source_uri
-        AND ca.rank = 1
-        INNER JOIN
-    SentimentLookup sl ON a.source_uri = sl.source_uri
-        INNER JOIN
-    NewsProviderComplete rp ON a.news_provider = rp.name
-		INNER JOIN
+	NewsArticles a
+	INNER JOIN
+	ClusterAssignment ca ON a.source_uri = ca.source_uri
+	AND ca.rank = 1
+	INNER JOIN
+	SentimentLookup sl ON a.source_uri = sl.source_uri
+	INNER JOIN
+	NewsProviderComplete rp ON a.news_provider = rp.name
+	INNER JOIN
 	Cluster c ON ca.cluster_id = c.cluster_id
 GROUP BY rp.root_name , c.cluster_name;
 
